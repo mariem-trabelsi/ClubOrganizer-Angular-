eac9d5509733bf708b98332b95f5e630278d6e7d
unknown <mariem.trabelsi@istic.ucar.tn>
2022-03-25 00:23:29 +0100

    ajout de 2 nouveaux composants

--- apps/my-app/src/app/app.module.ts
+++ apps/my-app/src/app/app.module.ts
@@ -15,6 +15,8 @@ import {FormsModule} from '@angular/forms';
 import{RouterModule,Routes} from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { LogoComponent } from './logo/logo.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { ParametresComponent } from './parametres/parametres.component';
 const appRoutes: Routes =[
   {path:'creerClub',component:CreerClubComponent},
   {path:'inscrireSite',component:InscrireSiteComponent},
@@ -22,6 +24,8 @@ const appRoutes: Routes =[
   {path:'clubsCards',component:CardsClubsComponent},
   {path:'sAuthentifier',component:SAuthentifierComponent},
   {path:'logo',component:LogoComponent},
+  {path:'meetings',component:MeetingsComponent},
+  {path:'parametres',component:ParametresComponent},
   {path:'abonnerClub',component:AbonnerClubComponent},
   {path:'',redirectTo:'/home',pathMatch:'full'}
 
@@ -37,7 +41,9 @@ const appRoutes: Routes =[
     SAuthentifierComponent,
     CardsClubsComponent,
     NavComponent,
-    LogoComponent
+    LogoComponent,
+    MeetingsComponent,
+    ParametresComponent
   ],
   imports: [
     BrowserModule,
