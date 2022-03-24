9154ca19174204ab68430dc52ff8160c65a75fa7
unknown <mariem.trabelsi@istic.ucar.tn>
2022-03-24 17:46:57 +0100

    data binding ''

--- apps/my-app/src/app/home/home.component.ts
+++ apps/my-app/src/app/home/home.component.ts
@@ -11,19 +11,17 @@ export class HomeComponent implements OnInit {
   publications=[];
   publication = {id:0, contenu:''};
   isAuth= false;
-  pub="salut  !, vu la derniere ..."
   
   addPub(){
     if(this.publication.contenu != ''){
 
     this.publication.id=this.publications.length +1;
-    this.publications.push()/*({
-      id:this.publication.id,
-      contenu:this.publication.contenu
-    })*/;
-   }
+    this.publications.push(/*{
+      id : this.publication.id,
+      contenu: this.publication.contenu}*/);
+    }
      this.publication.contenu='';
-          }
+    }
 
   constructor() { }
   ngOnInit(): void {}
--- apps/my-app/src/app/logo/logo.component.ts
+++ apps/my-app/src/app/logo/logo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-logo',
+  selector: '[app-logo]',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.css']
 })
