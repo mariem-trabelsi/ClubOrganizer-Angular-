a123f991fee54410e35744360efbbf5fc8f06ece
unknown <mariem.trabelsi@istic.ucar.tn>
2022-03-31 20:46:56 +0100

    no comment

--- apps/my-app/src/app/home/home.component.ts
+++ apps/my-app/src/app/home/home.component.ts
@@ -7,22 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  publications=[];
-  publication = {id:0, contenu:''};
   isAuth= false;
+  i=0
+  publication={"id":0,"contenu":""}
   
-  addPub(){
-    if(this.publication.contenu != ''){
+  addPub(event:Event)
+  {
+    var text=<HTMLInputElement> document.getElementById("zoneText")
+    this.publication.id=this.i
+    this.publication.contenu=text.value;
 
-    this.publication.id=this.publications.length +1;
-    this.publications.push();
-    }
-    /*{
-      id : this.publication.id,
-      contenu: this.publication.contenu}*/
-     this.publication.contenu='';
-    }
+    var div=<HTMLDivElement>document.getElementById("zoneAffichage")
+    div.innerHTML=div.innerHTML+` <br><div style="padding-left:130px;">
+    <div class="card" style="width: 70rem;">
+      <div class="card-body">
+        <h5 class="card-title">${this.publication.id}</h5>
+        <h6 class="card-subtitle mb-2 text-muted"></h6>
+        <p class="card-text" id="pub"> ${this.publication.contenu}</p>
+      </div>
+    </div>
+  </div> <br>`
+
+  this.i++;
+  text.value=""
+  }
 
   constructor() { }
   ngOnInit(): void {}
